61a23d79d5e55280e32fff265f20e8a9930d5f74
Daniel Wu <danwu@users.noreply.github.com>
2021-11-06 00:40:13 -0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+((refs) => {
+    // v9 major release do nothing
+    // v9.9 minor release update major
+    // v9.9.9 patch release update minor
+
+
+})([
+    "v1"
+    ,"v2.14"
+    ,"v2.14.3"
+    ,"v2.14.2"
+    ,"v2.8.2"
+    ,"v2"
+    ,"v2.a"
+    ,"v1.2"
+    ,"v1.2.1"
+    ,"v1.2.12"
+    ,"v1.3.12"
+])
+
